b1cabeed995ed2acf4d4f1b9699dc8f6b3770e79
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2026-05-13 10:52:43 -0400

    n

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 // Shay VanLandschoot
 // 5/11/26
 // Interactive Site
+
+
+// background color button starts here
+
+function darkButton (){
+    const dark = document.getElementById('darkmode');
+    dark.addEventListener('click', () => {
+        document.body.style.backgroundColor = 'rgba(37, 35, 35, 1)'
+        document.body.style.color = 'white' 
+    });
+}
+
+function lightButton (){
+    const light = document.getElementById('lightmode');
+    light.addEventListener('click', () => {
+        document.body.style.backgroundColor = 'rgba(255, 255, 255, 1)'
+        document.body.style.color = 'black'
+    });
+}
+
+lightButton();
+darkButton();
+
+// Background color buttons end here
---
+++ tictac.js
@@ -0,0 +1,3 @@
+// Shay VanLandschoot
+// 5/11/26
+// Interactive Site
